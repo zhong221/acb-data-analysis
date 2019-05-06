c2424db590cf772c293fc9de9bdb989645d07800
zhong221 <zhong221@umn.edu>
2019-05-06 17:48:28 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
-# Deploying your Dash app online
+# How to deploy a simple Plotly Dash app on Heroku
 
-1. Setup account on Heroku and download Heroku CLI utility
-2. Navigate to this folder
-3. Commit this folder to Git
-4. 'heroku login' and type in your credentials
-5. 'heroku create -n [YOUR-APP-NAME]' where YOUR-APP-NAME refers to the title of your Dash app
-6. 'heroku git:remote -a [YOUR-APP-GIT-URL]' where YOUR-APP-GIT-URL refers to the Git link returned by 5.
-7. 'git push heroku master' will deploy your app to Heroku
-8. 'heroku ps:scale web=1' will create a Dyno and make your app live
+A full explanation is available on [Medium](https://medium.com/@austinlasseter/how-to-deploy-a-simple-plotly-dash-app-to-heroku-622a2216eb73).
 
-If you want to make changes to your app repeat steps 2. 3. and 7.
+You can view the finished app on [Heroku](https://flying-dog.herokuapp.com/).
 
-Delete the runtime.txt if you wish to run on Python 2.7.x instead of 3.6.x
-
-Do NOT rename or delete any other file in this folder (except this README) or else your
-app will not setup properly.
+**Note:** Plotly Dash apps can only be viewed in a modern browser (like Chrome or Mozilla). They won't render in antediluvian browsers such as Microsoft.
--- app.py
+++ app.py
@@ -1,28 +1,44 @@
-# Import required libraries
-import os
-from random import randint
-
-import plotly.plotly as py
-from plotly.graph_objs import *
-
-import flask
 import dash
-from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.graph_objs as go
 
+########### Set up the chart
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 
-# Setup the app
-# Make sure not to change this file name or the variable names below,
-# the template is configured to execute 'server' on 'app.py'
-server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
+bitterness = go.Bar(
+    x=beers,
+    y=[35, 60, 85, 75],
+    name='IBU',
+    marker={'color':'red'}
+)
+alcohol = go.Bar(
+    x=beers,
+    y=[5.4, 7.1, 9.2, 4.3],
+    name='ABV',
+    marker={'color':'blue'}
+)
 
+beer_data = [bitterness, alcohol]
+beer_layout = go.Layout(
+    barmode='group',
+    title = 'Beer Comparison'
+)
 
-# Put your Dash code here
+beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
+########### Display the chart
 
-# Run the Dash app
+app = dash.Dash()
+server = app.server
+
+app.layout = html.Div(children=[
+    html.H1('Flying Dog Beers'),
+    dcc.Graph(
+        id='flyingdog',
+        figure=beer_fig
+    )]
+)
+
 if __name__ == '__main__':
-    app.server.run(debug=True, threaded=True)
+    app.run_server()
